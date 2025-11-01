aad2cc92eea1c4ac3681649d67753e47cbb368b4
John D. Harden <Jdharden123@gmail.com>
2025-11-01 16:59:37 -0700

    Refactor rodents.r for clarity and structure
    Removed redundant library imports and cleaned up the code structure. Added comments for clarity.

--- DC_RODENTS/rodents.r
+++ DC_RODENTS/rodents.r
@@ -1,21 +1,19 @@
+# loading packages
 library (tidyverse)
 library (ggplot2)
-library (tidyr)
-library (dplyr)
-library (readr) 
 library(raster)
-
-
-
+# Data lOad
 dc_rats <- read.csv ("2018_311 Rodent Calls.csv", stringsAsFactors=FALSE, na.strings="#VALUE!", header = TRUE)
-dc<-subset(us,NAME_1=="District of Columbia", level=5)
+dc <-subset(us,NAME_1=="District of Columbia", level=5)
 unique(us$NAME_1)
-
 
 plot(dc)
 head(dc_rats)
 summary(dc_rats)
 str(dc_rats)
 
+#-------
+# visualize the data
 ggplot ( data = dc_rats ) + 
 geom_point ( mapping = aes ( x = LATITUDE , y = LONGITUDE, color = WARD))
+
